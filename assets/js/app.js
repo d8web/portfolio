5b781274b4698bf8f82e69d58deb98dfe797fbe2
Daniel <df46636@gmail.com>
2021-02-01 15:20:51 -0300

    Header 95%

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,11 @@
-function handleMenu() {
-    
+function handleMenuOpen(element) {
+    element.style.display = 'none'
+    document.querySelector('.btn-menu-close').style.display = 'block'
+    document.querySelector('.navbar .desktop').style.left = '0'
+}
+
+function handleCloseMenu(element) {
+    element.style.display = 'none'
+    document.querySelector('.btn-menu-open').style.display = 'block'
+    document.querySelector('.navbar .desktop').style.left = '-1000px'
 }
